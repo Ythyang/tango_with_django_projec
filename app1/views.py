779017797007b6f3f959071b32e0f599dd92d61c
Ythyang <2681610y@student.gla.ac.uk>
2022-02-11 04:31:37 +0800

    c4

--- app1/views.py
+++ app1/views.py
@@ -3,7 +3,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 app_name= 'app1'
 def about(request):
-    return HttpResponse("Rango1 says hey there partner!")
     
-
-# Create your views here.
+    return render(request, 'about.html')
